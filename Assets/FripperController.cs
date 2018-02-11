24eeefdafa833018ca7b7e78d8379fe9ce91d2fa
jingarn <jingarn@gmail.com>
2018-02-12 02:50:40 +0900

    PinBall 課題提出用
    初コミット

---
+++ Assets/FripperController.cs
@@ -0,0 +1,162 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour
+{
+    //HingiJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start()
+    {
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TouchInfo touchInfo = AppUtil.GetTouch();
+        Vector3 touchPosition = AppUtil.GetTouchPosition();
+
+        //左矢印キーを押した時左フリッパーを動かす
+        if ((touchInfo == TouchInfo.Began && Screen.width * 0.5f >= touchPosition[0] || Input.GetKeyDown(KeyCode.LeftArrow)) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+
+        //右矢印キーを押した時右フリッパーを動かす
+        if ((touchInfo == TouchInfo.Began && Screen.width * 0.5f <= touchPosition[0] || Input.GetKeyDown(KeyCode.RightArrow)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キー離された時フリッパーを元に戻す
+        if ((touchInfo == TouchInfo.Ended || Input.GetKeyUp(KeyCode.LeftArrow)) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if ((touchInfo == TouchInfo.Ended || Input.GetKeyUp(KeyCode.RightArrow)) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+    }
+
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+        JointSpring jointSpr = this.myHingeJoint.spring;
+        jointSpr.targetPosition = angle;
+        this.myHingeJoint.spring = jointSpr;
+    }
+
+}
+
+/// <summary>
+/// 参考 https://qiita.com/tempura/items/4a5482ff6247ec8873df
+/// </summary>
+public static class AppUtil
+{
+    private static Vector3 TouchPosition = Vector3.zero;
+
+    /// <summary>
+    /// タッチ情報を取得(エディタと実機を考慮)
+    /// </summary>
+    /// <returns>タッチ情報。タッチされていない場合は null</returns>
+    public static TouchInfo GetTouch()
+    {
+        if (Application.isEditor)
+        {
+            if (Input.GetMouseButtonDown(0)) { return TouchInfo.Began; }
+            if (Input.GetMouseButton(0)) { return TouchInfo.Moved; }
+            if (Input.GetMouseButtonUp(0)) { return TouchInfo.Ended; }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                return (TouchInfo)((int)Input.GetTouch(0).phase);
+            }
+        }
+        return TouchInfo.None;
+    }
+
+    /// <summary>
+    /// タッチポジションを取得(エディタと実機を考慮)
+    /// </summary>
+    /// <returns>タッチポジション。タッチされていない場合は (0, 0, 0)</returns>
+    public static Vector3 GetTouchPosition()
+    {
+        if (Application.isEditor)
+        {
+            TouchInfo touch = AppUtil.GetTouch();
+            if (touch != TouchInfo.None) { return Input.mousePosition; }
+        }
+        else
+        {
+            if (Input.touchCount > 0)
+            {
+                Touch touch = Input.GetTouch(0);
+                TouchPosition.x = touch.position.x;
+                TouchPosition.y = touch.position.y;
+                return TouchPosition;
+            }
+        }
+        return Vector3.zero;
+    }
+
+    /// <summary>
+    /// タッチワールドポジションを取得(エディタと実機を考慮)
+    /// </summary>
+    /// <param name='camera'>カメラ</param>
+    /// <returns>タッチワールドポジション。タッチされていない場合は (0, 0, 0)</returns>
+    public static Vector3 GetTouchWorldPosition(Camera camera)
+    {
+        return camera.ScreenToWorldPoint(GetTouchPosition());
+    }
+}
+
+/// <summary>
+/// タッチ情報。UnityEngine.TouchPhase に None の情報を追加拡張。
+/// </summary>
+public enum TouchInfo
+{
+    /// <summary>
+    /// タッチなし
+    /// </summary>
+    None = 99,
+
+    // 以下は UnityEngine.TouchPhase の値に対応
+    /// <summary>
+    /// タッチ開始
+    /// </summary>
+    Began = 0,
+    /// <summary>
+    /// タッチ移動
+    /// </summary>
+    Moved = 1,
+    /// <summary>
+    /// タッチ静止
+    /// </summary>
+    Stationary = 2,
+    /// <summary>
+    /// タッチ終了
+    /// </summary>
+    Ended = 3,
+    /// <summary>
+    /// タッチキャンセル
+    /// </summary>
+    Canceled = 4,
+}
---
+++ Assets/ScoreManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    private static readonly Dictionary<string, int> PointDictionary = new Dictionary<string, int>()
+    {
+        { "SmallStarTag", 1 },
+        {"LargeStarTag", 3 },
+        { "SmallCloudTag", 4 },
+        { "LargeCloudTag", 7 }
+    };
+
+    //スコアを表示するテキスト
+    private GameObject scoreText;
+
+    // 角度
+    private static int score = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //シーン中のスコアオブジェクトを取得
+        this.scoreText = GameObject.Find("ScoreText");
+        this.scoreText.GetComponent<Text>().text = 0.ToString();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    //衝突時に呼ばれる関数
+    void OnCollisionEnter(Collision other)
+    {
+        //スコア更新
+        score += PointDictionary[this.tag];
+        this.scoreText.GetComponent<Text>().text = score.ToString();
+    }
+
+}
